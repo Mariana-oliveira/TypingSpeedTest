15e891a25c3d20157649e46e86ab820dbe54200f
Mariana <mariana_oliveira_9@hotmail.com>
2020-05-09 12:32:01 +0100

    error counters, final modal with results

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,25 +4,66 @@ const ERRORS = document.querySelector(".errors");
 const RESET = document.querySelector(".reset");
 const REFRESH_TEXT = document.querySelector(".refresh-text");
 const TEXT_P = document.querySelector(".text-to-type");
-const TEXT_TO_TYPE = document.querySelector(".text-to-type ").innerHTML;
+const MODAL = document.querySelector(".modal");
+const CLOSE_MODAL_BUTTON = document.querySelector(".close-button");
+const OVERLAY = document.querySelector("#overlay");
+const MODAL_TIME = document.querySelector(".modal-time");
+const MODAL_ERRORS = document.querySelector(".modal-errors");
+const MODAL_WORDS = document.querySelector(".modal-words");
+const MODAL_CHAR = document.querySelector(".modal-char");
+const MODAL_ACCURACY = document.querySelector(".modal-accuracy");
 
 
+// Global variables
 var time = [0,0,0,0];  //min, sec, csec, csec
 var timerRunning = false;
 var totalErrors = 0;
-var typedLength = [0,0]; // min, max
 var interval;
 
+// Array with quotes to type - set he first to the element
 var textArray = [
-	"Text to type.", 
-	"The most common way people give up their power is by thinking they don’t have any.", 
+	"The most common way people give up their power is by thinking they don't have any.", 
 	"Great things never come from comfort zones.",
 	"Push yourself, because no one else is going to do it for you.",
 	"It's going to be hard, but hard does not mean impossible.",
 	"Learning never exhausts the mind.",
-  	"The only way to do great work is to love what you do."
+	"The only way to do great work is to love what you do."
 ];
 var currentText = 0;
+var TEXT_TO_TYPE = textArray[currentText];
+TEXT_P.innerHTML = textArray[currentText];
+
+
+
+
+// Open Modal 
+function OpenModal(){
+	MODAL.classList.add("active");
+	OVERLAY.classList.add("active");
+}
+
+//Close Modal
+function CloseModal(){
+	MODAL.classList.remove("active");
+	OVERLAY.classList.remove("active");
+	ResetTimer();
+}
+
+//Populate Modal with info
+function PopulateModal(){
+	let words = Math.round(textArray[currentText].split(" ").length / ((time[3]/100) / 60));
+	let chars = Math.round(textArray[currentText].length / ((time[3]/100) / 60));
+	let accuracy = Math.round( (1 - totalErrors / textArray[currentText].length) * 100);
+	console.log(time[0]);
+
+	MODAL_TIME.innerHTML = TIME.innerText;
+	MODAL_ERRORS.innerHTML = totalErrors;
+	MODAL_WORDS.innerHTML = words;
+	MODAL_CHAR.innerHTML = chars;
+	MODAL_ACCURACY.innerHTML = accuracy + "%";
+}
+
+
 
 //Function that adds zero leading
 function LeadingZero(time){
@@ -31,7 +72,6 @@ function LeadingZero(time){
 	}
 	return time;
 }
-
 
 // Function that counts the time running:
 function CountingTime(){
@@ -44,20 +84,21 @@ function CountingTime(){
 }
 
 //Function to count errors
-function ErrorCounter(textTyped, originalText){
-	
-	let errorCount =0
+function ErrorCounter(typedText, originalText){
 
-	for(i = 0; i < textTyped.length; i++){
-		textTyped[i] != originalText[i] ? errorCount++ : errorCount;
+	let length = typedText.length -1;
+
+	if( typedText[length] != originalText[length] ){
+		totalErrors++;
+		ERRORS.textContent = totalErrors;
 	}
-	totalErrors += errorCount;
-	ERRORS.textContent = totalErrors;
+
 }
 
 
 // Function that starts the timer:
 function StartTime(){
+
 	if(!timerRunning && TEXTAREA.value.length === 0){
 		timerRunning = true;
 		interval = setInterval(CountingTime, 10);
@@ -66,14 +107,18 @@ function StartTime(){
 
 
 // Function to check if the text entered matches the text to type: 
-function CheckSpelling(){
+function CheckSpelling(e){
 	let textTyped = TEXTAREA.value;
 	let originalTextSub = TEXT_TO_TYPE.substring(0, textTyped.length);
+
+	let key = e.keyCode || e.CharCode;
 
 	if(textTyped == TEXT_TO_TYPE)
 	{
 		clearInterval(interval);
 		TEXTAREA.style.borderColor = "#429890";
+		PopulateModal();
+		OpenModal();
 
 	} else if(textTyped == originalTextSub){
 
@@ -81,28 +126,10 @@ function CheckSpelling(){
 		
 	}else{
 		TEXTAREA.style.borderColor = "#EA4335";
-				
-		
-		// To be sure that when deleting it's not counting errors
-
-
-		if(typedLength[1] < textTyped.length){
-			typedLength[1] = textTyped.length;
-			typedLength[0] = textTyped.length;
-		}else{
-			typedLength[0] = textTyped.length;
-		}
-
-		// if(textTyped.length >= typedLength[0])
-
-		if(typedLength[0] == typedLength[1]){
+			
+		if( ![8,37,38,39,40,46].includes(key)){	
 			ErrorCounter(textTyped, originalTextSub);
 		}
-
-		console.log("Current: " + textTyped.length);
-		console.log("MAX: " + typedLength[1]);
-		console.log("MIN: " + typedLength[0]);
-		
 	}
 
 }
@@ -125,11 +152,11 @@ function ResetTimer(){
 
 // Function to change the text to type
 function ChangeTextToType(){
-	ResetTimer()
+	ResetTimer();
 	currentText < (textArray.length-1) ? currentText++ : currentText =0;
-	console.log(textArray.length);
-	console.log(currentText);
+
 	TEXT_P.innerHTML = textArray[currentText];
+	TEXT_TO_TYPE = textArray[currentText];
 }
 
 
@@ -140,3 +167,5 @@ TEXTAREA.addEventListener("keypress", StartTime, false);
 TEXTAREA.addEventListener("keyup", CheckSpelling, false);
 RESET.addEventListener("click", ResetTimer, false);
 REFRESH_TEXT.addEventListener("click", ChangeTextToType, false);
+CLOSE_MODAL_BUTTON.addEventListener("click", CloseModal, false);
+OVERLAY.addEventListener("click", CloseModal, false);
